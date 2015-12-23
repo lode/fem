029177450c825002c9483dc2f4fa5c4eb45e3703
Lode Claassen <lode@alsvanzelf.nl>
2015-12-23 23:24:39 +0100

    allow to check for updates composer packages

--- src/build.php
+++ src/build.php
@@ -34,4 +34,77 @@ public static function database_dump_structure($dump_directory) {
 	}
 }
 
+/**
+ * checks composer packages for new releases since the installed version
+ * 
+ * @todo check if the required version is significantly off
+ *       which would mean the json needs to change to be able to update
+ * 
+ * @return array {
+ *         @var $required
+ *         @var $installed
+ *         @var $possible
+ * }
+ */
+public static function check_composer_updates() {
+	$composer_json  = file_get_contents(ROOT_DIR.'/composer.json');
+	$composer_json = json_decode($composer_json, true);
+	if (empty($composer_json['require'])) {
+		$exception = bootstrap::get_library('exception');
+		throw new $exception('there are no required packages to check');
+	}
+	
+	$composer_lock = file_get_contents(ROOT_DIR.'/composer.lock');
+	$composer_lock = json_decode($composer_lock, true);
+	if (empty($composer_lock['packages'])) {
+		$exception = bootstrap::get_library('exception');
+		throw new $exception('lock file is missing its packages');
+	}
+	
+	$required_packages  = $composer_json['require'];
+	$installed_packages = $composer_lock['packages'];
+	$update_packages    = array();
+	
+	foreach ($installed_packages as $installed_package) {
+		$package_name      = $installed_package['name'];
+		$installed_version = preg_replace('/v([0-9].*)/', '$1', $installed_package['version']);
+		$version_regex     = '/versions\s*:.+v?([0-9]+\.[0-9]+(\.[0-9]+)?)(,|$)/U';
+		
+		// skip dependencies of dependencies
+		if (empty($required_packages[$package_name])) {
+			continue;
+		}
+		
+		// check commit hash for dev-* versions
+		if (strpos($installed_version, 'dev-') === 0) {
+			$installed_version = $installed_package['source']['reference'];
+			$version_regex     = '/source\s*:.+ ([a-f0-9]{40})$/m';
+		}
+		
+		// find out the newest release
+		$package_info = shell_exec('composer show -a '.escapeshellarg($package_name));
+		preg_match($version_regex, $package_info, $possible_version);
+		if (empty($possible_version)) {
+			$exception = bootstrap::get_library('exception');
+			throw new $exception('can not find out newest release for '.$package_name);
+		}
+		
+		if (ENVIRONMENT == 'development') {
+			echo 'installed '.$package_name.' at '.$installed_version.', possible version is '.$possible_version[1].PHP_EOL;
+		}
+		
+		if ($possible_version[1] == $installed_version) {
+			continue;
+		}
+		
+		$update_packages[$package_name] = array(
+			'required'  => $required_packages[$package_name],
+			'installed' => $installed_version,
+			'possible'  => $possible_version[1],
+		);
+	}
+	
+	return $update_packages;
+}
+
 }
